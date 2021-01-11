1408a90f82ab3be0062d3f117ef7037e1ecd3638
Bryce St. Pierre <brycelstpierre@gmail.com>
2021-01-11 15:41:07 -0500

    Replaced single quotes with double quotes.

--- category.php
+++ category.php
@@ -13,11 +13,11 @@
       <h1 class="display-6 mb-0 font-italic"><?php single_cat_title(); ?></h1>
       <hr>
       <?php get_template_part( "template-parts/page/list-posts" ); ?>
-      <p class='text-center'>
-        <?php get_template_part( 'template-parts/page/paginate-links' ); ?>
+      <p class="text-center">
+        <?php get_template_part( "template-parts/page/paginate-links" ); ?>
       </p>
     </main>
-    <aside class='col-md-4'>
+    <aside class="col-md-4">
       <?php get_sidebar(); ?>
     </aside>
   </div>
